f2b901988e00ebf5e8b6552ff9205cc4b8646c24
Mark Nelson <mcb419@gmail.com>
2018-12-01 13:43:44 -0600

    added event-loop code

--- p5lite.js
+++ p5lite.js
@@ -8,11 +8,64 @@
 // * circle(x, y, r)
 // * csave() and crestore() instead of push() and pop() for context
 
-// TODO: event loop, frameRate, 
-// TODO: translate, rotate, scale
 // TODO: seedable random numbers
 
 var height, width; //globals exposed
+
+//============
+// EVENT LOOP
+//============
+
+(function(e) {
+
+  var FPS = 60; // frameRate (frames per second)
+  var UPT = 1; // updatesPerTick
+  var paused;
+
+  e.p5setup = function() {
+    paused = true;
+    e.setup(); // user
+    if(!e.gcnv())e.createCanvas(300, 100); // default if user didn't create a canvas
+    e.reset(); //user
+    e.draw(); // user
+  };
+
+  e.p5pause = () => {paused = true;};
+  e.p5reset = () => {paused = true; e.reset(); };
+  e.p5run =  () => { paused = false; e.p5tick(); };
+  e.p5step = () => {paused = true; e.update(); e.draw();};
+  e.p5tick = function() {
+    if (!paused) {
+      setTimeout(e.p5tick, 1000 / FPS);
+      for (let i = 0; i < UPT; i++) e.update();
+    }
+    e.draw();
+  };
+
+  e.frameRate = (rate) => rate ? FPS = rate : FPS;
+  e.isPaused = () => paused;
+  e.loop = e.p5run;
+  e.noLoop = e.p5pause;
+  e.redraw = () => { paused = true; e.draw(); };
+  e.updatesPerTick = (num) => num ? UPT = num : UPT;
+
+  // empty versions of user-defined functions
+  e.draw = () => {};
+  e.reset = () => {};
+  e.setup = () => {};
+  e.update = () => {};
+
+  // call p5setup once DOM is loaded
+  if (document.readyState === 'complete') {
+    e.p5setup();
+  } else {
+    document.addEventListener('DOMContentLoaded', function() {
+      document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+      e.p5setup();
+    }, false);
+  }
+  
+})(window);
 
 //===========
 // GRAPHICS
@@ -61,11 +114,11 @@ var height, width; //globals exposed
     // set globals
     height = cnv.height;
     width = cnv.width;
-  }
+  };
 
   e.createCanvas = function (w, h, id = 'p5') {
     e.canvas(id, { width: w, height: h });
-  }
+  };
 
   e.background = function (r, g, b, a = 1.0) {
     var colorSpec;
@@ -79,7 +132,7 @@ var height, width; //globals exposed
     }
     ctx.fillStyle = colorSpec;
     ctx.fillRect(0, 0, width, height);
-  }
+  };
 
   e.circle = function (x, y, r) {
     ctx.beginPath();
@@ -87,15 +140,15 @@ var height, width; //globals exposed
     ctx.closePath();
     if (ctx._doStroke) ctx.stroke();
     if (ctx._doFill) ctx.fill();
-  }
+  };
 
   e.crestore = function () {
     ctx.restore();
-  }
+  };
 
   e.csave = function () {
     ctx.save();
-  }
+  };
 
   e.ellipse = function (x, y, w, h = w) {
     ctx.beginPath();
@@ -103,7 +156,7 @@ var height, width; //globals exposed
     ctx.closePath();
     if (ctx._doFill) ctx.fill();
     if (ctx._doStroke) ctx.stroke();
-  }
+  };
 
   e.fill = function (r, g, b, a = 1.0) {
     var colorSpec;
@@ -117,16 +170,16 @@ var height, width; //globals exposed
     }
     ctx._doFill = true;
     ctx.fillStyle = colorSpec;
-  }
+  };
 
   e.line = function (x1, y1, x2, y2) {
-    if (!ctx._doStroke) return
+    if (!ctx._doStroke) return;
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.closePath();
-  }
+  };
 
   e.lineWidth = function (w) {
     if (typeof w === 'undefined' || w === 0) {
@@ -135,16 +188,16 @@ var height, width; //globals exposed
     } else {
       ctx.lineWidth = w;
     }
-  }
+  };
 
 
   e.noFill = function () {
     ctx._doFill = false;
-  }
+  };
 
   e.noStroke = function () {
     ctx._doStroke = false;
-  }
+  };
 
   e.rect = function (x, y, w, h) {
     ctx.beginPath();
@@ -152,7 +205,15 @@ var height, width; //globals exposed
     ctx.closePath();
     if (ctx._doFill) ctx.fill();
     if (ctx._doStroke) ctx.stroke();
-  }
+  };
+
+  e.rotate = function(angle) {
+    ctx.rotate(angle);
+  };
+
+  e.scale = function(sx, sy=sx) {
+    ctx.scale(sx, sy);
+  };
 
   e.stroke = function (r, g, b, a = 1.0) {
     var colorSpec;
@@ -163,19 +224,23 @@ var height, width; //globals exposed
     } else {
       if (a > 1) a /= 255; // allows alpha 0-255 (p5 style)
       colorSpec = `rgba(${r}, ${g}, ${b}, ${a})`;
+
     }
     ctx._doStroke = true;
     ctx.strokeStyle = colorSpec;
-  }
+  };
 
   e.strokeWeight = function (w) {
     this.lineWidth(w);
-  }
+  };
 
   e.text = function (text, x, y) {
     if (ctx._doFill) ctx.fillText(text, x, y);
-    if (ctx._doStroke) ctx.strokeText(text, x, y);
-  }
+  };
+
+  e.translate = function(x, y) {
+    ctx.translate(x, y);
+  };
 
 
 })(window);
@@ -185,11 +250,11 @@ var height, width; //globals exposed
 //===========
 (function (e) {
   e.PI = Math.PI;
-  e.TWO_PI = 2 * PI;
-  e.HALF_PI = PI / 2;
-  e.QUARTER_PI = PI / 4;
-  e.DEG_TO_RAD = PI / 180;
-  e.RAD_TO_DEG = 180 / PI;
+  e.TWO_PI = 2 * e.PI;
+  e.HALF_PI = e.PI / 2;
+  e.QUARTER_PI = e.PI / 4;
+  e.DEG_TO_RAD = e.PI / 180;
+  e.RAD_TO_DEG = 180 / e.PI;
 
   e.abs = x => Math.abs(x);
   e.acos = x => Math.acos(x);
